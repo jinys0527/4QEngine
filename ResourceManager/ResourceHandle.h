0d751b0186d2d6db455f2561a533871f18fa1ad2
jinys0527 <jinys0527@naver.com>
2025-12-27 22:48:30 +0900

    일단 ResourceManager 틀 잡기 컴포넌트, 유틸함수 -> Assimp 이후에 Resource Manager Load를 완성해야될듯

---
+++ ResourceManager/ResourceHandle.h
@@ -0,0 +1,74 @@
+﻿#pragma once
+
+#include <cstdint>
+
+// id + generation만 들고 있는 가벼운 참조값
+
+struct ResourceHandle
+{
+	uint32_t id = 0;
+	uint32_t generation = 0;
+
+	bool IsValid() const
+	{
+		return id != 0;
+	}
+
+	static ResourceHandle Invalid()
+	{
+		return {};
+	}
+
+	bool operator==(const ResourceHandle& other) const
+	{
+		return id == other.id && generation == other.generation;
+	}
+
+	bool operator!=(const ResourceHandle& other) const
+	{
+		return !(*this == other);
+	}
+};
+
+template <typename Tag>
+struct TypedHandle
+{
+	uint32_t id = 0;
+	uint32_t generation = 0;
+
+	bool IsValid() const
+	{
+		return id != 0;
+	}
+
+	static TypedHandle Invalid()
+	{
+		return {};
+	}
+
+	bool operator==(const TypedHandle& other) const
+	{
+		return id == other.id && generation == other.generation;
+	}
+
+	bool operator!=(const TypedHandle& other) const
+	{
+		return !(*this == other);
+	}
+};
+
+struct MeshTag
+{
+};
+
+struct MaterialTag
+{
+};
+
+struct TextureTag
+{
+};
+
+using MeshHandle     = TypedHandle<MeshTag>;
+using MaterialHandle = TypedHandle<MaterialTag>;
+using TextureHandle  = TypedHandle<TextureTag>;
