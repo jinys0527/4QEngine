40281faad34fb0f7e96a5288200dfbea9d003f24
User <honghan3235@gmail.com>
2026-01-21 21:51:16 +0900

    Done Today

---
+++ GameObject/SceneChangeTestComponent.cpp
@@ -0,0 +1,63 @@
+﻿#include "SceneChangeTestComponent.h"
+#include "ReflectionMacro.h"
+#include "ServiceRegistry.h"
+#include "Object.h"
+#include "Event.h"
+#include "Scene.h"
+
+
+REGISTER_COMPONENT(SceneChangeTestComponent)
+
+SceneChangeTestComponent::SceneChangeTestComponent()
+{
+
+}
+
+SceneChangeTestComponent::~SceneChangeTestComponent()
+{
+	GetEventDispatcher().RemoveListener(EventType::KeyDown, this);
+	GetEventDispatcher().RemoveListener(EventType::KeyUp, this);
+}
+
+void SceneChangeTestComponent::Start()
+{
+	GetEventDispatcher().AddListener(EventType::KeyDown, this); // 이벤트 추가
+	GetEventDispatcher().AddListener(EventType::KeyUp, this);
+}
+
+void SceneChangeTestComponent::Update(float deltaTime)
+{
+	if (!GetOwner()->GetScene())
+		return;
+	if (m_IsF12) {
+
+		SceneChange();
+	}
+}
+
+void SceneChangeTestComponent::OnEvent(EventType type, const void* data)
+{
+	if (type == EventType::KeyUp) {
+		auto keyData = static_cast<const Events::KeyEvent*>(data);
+		if (!keyData) return;
+		bool isDown = (type == EventType::KeyDown);
+		switch (keyData->key) {
+		
+		case VK_F12: m_IsF12 = isDown; break;
+		default:break;
+
+		}
+	}
+}
+
+void SceneChangeTestComponent::SceneChange()
+{
+	Scene* scene = GetOwner()->GetScene();
+	if (!scene)
+		return;
+
+	auto& services = scene->GetServices();
+	auto& gameManager = services.Get<GameManager>();
+
+	//gameManager.RequestSceneChange("Game Test2");
+}
---
+++ GameObject/SceneChangeTestComponent.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Component.h"
+
+class SceneChangeTestComponent : public Component, public IEventListener {
+
+	friend class Editor;
+
+public :
+	static constexpr const char* StaticTypeName = "SceneChangeTestComponent";
+	const char* GetTypeName() const override;
+
+	SceneChangeTestComponent();
+	virtual ~SceneChangeTestComponent();
+
+	void Start() override;
+
+	void Update(float deltaTime) override;
+	void OnEvent(EventType type, const void* data) override;
+
+	void SceneChange();
+private:
+	bool m_IsF12 = false;
+};
