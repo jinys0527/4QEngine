37609089b260ab2d8c6698f701d5de6ea91a8874
User <honghan3235@gmail.com>
2026-01-22 21:27:22 +0900

    Client Recent

---
+++ GameObject/ComponentTemplate.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+/// 만들기 귀찮아서 만들어둔 Template(c++ Template 아님)
+/// 복붙해서 이름 바꿔서 쓰세요
+#include "Component.h"
+
+// 이벤트 리스너는 쓸 얘들만
+
+
+class EmptyComponent : public Component, public IEventListener {
+	friend class Editor;
+public:
+	static constexpr const char* staticTypeName = "EmptyComponent";
+
+	EmptyComponent();
+	virtual ~EmptyComponent();
+
+	void Start() override;
+
+	void Update(float deltaTime) override;
+	void OnEvent(EventType type, const void* data) override; // IEventListener 필요
+
+private:
+
+
+};
