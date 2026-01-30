afa4d30a6206316714e4274e52f39ec2787cfc51
User <honghan3235@gmail.com>
2026-01-30 19:48:02 +0900

    Merge Tri

--- GameObject/EnemyMovementComponent.cpp
+++ GameObject/EnemyMovementComponent.cpp
@@ -122,6 +122,12 @@ void EnemyMovementComponent::Move()
 
 
 
+void EnemyMovementComponent::SetEnemyRotation(TransformComponent* transComp, RotationOffset dir)
+{
+
+}
+
+
 void EnemyMovementComponent::GetSystem()
 {
 	auto* scene = GetOwner() -> GetScene();
