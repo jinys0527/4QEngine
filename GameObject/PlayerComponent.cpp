9da0ea0328bada25df7c8f172eeacb21f2465bee
DotheZac <128376783+DotheZac@users.noreply.github.com>
2026-02-12 14:37:50 +0900

    근접무기 해제되었을 때 행동포인트 남던 버그 수정

--- GameObject/PlayerComponent.cpp
+++ GameObject/PlayerComponent.cpp
@@ -1400,50 +1400,85 @@ void PlayerComponent::Update(float deltaTime) {
 
 
 	//근접 무기 스탯 적용
+	auto resetMeleeCombatStats = [&]()
+		{
+			m_AttackRange = 1;
+			m_CurrentWeaponCost = 1;
+
+			auto* playerstatcomponent = owner->GetComponent<PlayerStatComponent>();
+			if (!playerstatcomponent)
+			{
+				return;
+			}
+
+			playerstatcomponent->SetRange(1);
+			playerstatcomponent->SetEquipmentDefenseBonus(0);
+			playerstatcomponent->SetEquipmentHealthBonus(0);
+			playerstatcomponent->SetEquipmentStrengthBonus(0);
+			playerstatcomponent->SetEquipmentAgilityBonus(0);
+			playerstatcomponent->SetEquipmentSenseBonus(0);
+			playerstatcomponent->SetEquipmentSkillBonus(0);
+
+			m_IsApplyMeleeStat = false;
+		};
+
 	if (m_MeleeItem != nullptr)
 	{
 		auto* itemcomponent = m_MeleeItem->GetComponent<ItemComponent>();
-		if (!itemcomponent) return;
-
-		// 근접 무기 전투값 동기화 (사거리/AP 소모)
-		m_AttackRange = max(0, itemcomponent->GetMeleeAttackRange());
-		m_CurrentWeaponCost = max(0, itemcomponent->GetActionPointCost());
-
-		//근접 무기의 스탯 적용하기
-		if (!m_IsApplyMeleeStat)
+		if (!itemcomponent)
 		{
-			auto* playerstatcomponent = owner->GetComponent<PlayerStatComponent>();
-			if (!playerstatcomponent) return;
-
-			int health = playerstatcomponent->GetHealth();
-			int strength = playerstatcomponent->GetStrength();
-			int agility = playerstatcomponent->GetAgility();
-			int sense = playerstatcomponent->GetSense();
-			int skill = playerstatcomponent->GetSkill();
-
-			int ihealth = itemcomponent->GetHealth();
-			int istrength = itemcomponent->GetStrength();
-			int iagility = itemcomponent->GetAgility();
-			int isense = itemcomponent->GetSense();
-			int iskill = itemcomponent->GetSkill();
-			int idefense = itemcomponent->GetDEF();
-			int irange = itemcomponent->GetMeleeAttackRange();
-
-			playerstatcomponent->SetHealth(health);
-			playerstatcomponent->SetStrength(strength);
-			playerstatcomponent->SetAgility(agility);
-			playerstatcomponent->SetSense(sense);
-			playerstatcomponent->SetSkill(skill);
-			playerstatcomponent->SetEquipmentDefenseBonus(idefense);
-			playerstatcomponent->SetRange(static_cast<int>(irange));
-			playerstatcomponent->SetEquipmentHealthBonus(ihealth);
-			playerstatcomponent->SetEquipmentStrengthBonus(istrength);
-			playerstatcomponent->SetEquipmentAgilityBonus(iagility);
-			playerstatcomponent->SetEquipmentSenseBonus(isense);
-			playerstatcomponent->SetEquipmentSkillBonus(iskill);
-
-			m_IsApplyMeleeStat = true;
+			resetMeleeCombatStats();
 		}
+		else if (itemcomponent->GetIsEquiped())
+		{
+			// 근접 무기 전투값 동기화 (사거리/AP 소모)
+			m_AttackRange = max(0, itemcomponent->GetMeleeAttackRange());
+			m_CurrentWeaponCost = max(0, itemcomponent->GetActionPointCost());
+
+			//근접 무기의 스탯 적용하기
+			if (!m_IsApplyMeleeStat)
+			{
+				auto* playerstatcomponent = owner->GetComponent<PlayerStatComponent>();
+				if (!playerstatcomponent) return;
+
+				int health = playerstatcomponent->GetHealth();
+				int strength = playerstatcomponent->GetStrength();
+				int agility = playerstatcomponent->GetAgility();
+				int sense = playerstatcomponent->GetSense();
+				int skill = playerstatcomponent->GetSkill();
+
+				int ihealth = itemcomponent->GetHealth();
+				int istrength = itemcomponent->GetStrength();
+				int iagility = itemcomponent->GetAgility();
+				int isense = itemcomponent->GetSense();
+				int iskill = itemcomponent->GetSkill();
+				int idefense = itemcomponent->GetDEF();
+				int irange = itemcomponent->GetMeleeAttackRange();
+
+				playerstatcomponent->SetHealth(health);
+				playerstatcomponent->SetStrength(strength);
+				playerstatcomponent->SetAgility(agility);
+				playerstatcomponent->SetSense(sense);
+				playerstatcomponent->SetSkill(skill);
+				playerstatcomponent->SetEquipmentDefenseBonus(idefense);
+				playerstatcomponent->SetRange(static_cast<int>(irange));
+				playerstatcomponent->SetEquipmentHealthBonus(ihealth);
+				playerstatcomponent->SetEquipmentStrengthBonus(istrength);
+				playerstatcomponent->SetEquipmentAgilityBonus(iagility);
+				playerstatcomponent->SetEquipmentSenseBonus(isense);
+				playerstatcomponent->SetEquipmentSkillBonus(iskill);
+
+				m_IsApplyMeleeStat = true;
+			}
+		}
+		else
+		{
+			resetMeleeCombatStats();
+		}
+	}
+	else
+	{
+		resetMeleeCombatStats();
 	}
 
 	// 장착 무기 표시/숨김: CombatMode에 따라 근접, 던지기, 없음(Idle)
